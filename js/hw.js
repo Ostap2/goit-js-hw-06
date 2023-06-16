fe4d6bb8bf2434c8c1db7d50f347d10dd2c00466
Ostap <Ostapkriven13@gmail.com>
2023-06-16 19:46:16 +0300

    Update hw.js

--- js/hw.js
+++ js/hw.js
@@ -1,62 +1,62 @@
-// 1
-const categoriesList = document.querySelector('#categories');
-const categories = categoriesList.querySelectorAll('.item');
+// // 1
+// const categoriesList = document.querySelector('#categories');
+// const categories = categoriesList.querySelectorAll('.item');
 
-console.log('Number of categories:', categories.length);
+// console.log('Number of categories:', categories.length);
 
-categories.forEach(category => {
-  const categoryName = category.querySelector('h2').textContent;
-  const categoryElements = category.querySelectorAll('li');
+// categories.forEach(category => {
+//   const categoryName = category.querySelector('h2').textContent;
+//   const categoryElements = category.querySelectorAll('li');
 
-  console.log('Category:', categoryName);
-  console.log('Elements:', categoryElements.length);
-});
+//   console.log('Category:', categoryName);
+//   console.log('Elements:', categoryElements.length);
+// });
 
 
-// 2
+// // 2
 
-const ingredients = [
-  "Potatoes",
-  "Mushrooms",
-  "Garlic",
-  "Tomatos",
-  "Herbs",
-  "Condiments",
-];
+// const ingredients = [
+//   "Potatoes",
+//   "Mushrooms",
+//   "Garlic",
+//   "Tomatos",
+//   "Herbs",
+//   "Condiments",
+// ];
 
-const ingredientsList = document.getElementById('ingredients');
+// const ingredientsList = document.getElementById('ingredients');
 
-ingredients.forEach(ingredient => {
-  const listItem = document.createElement('li');
-  listItem.textContent = ingredient;
-  listItem.classList.add('item');
-  ingredientsList.appendChild(listItem);
-});
+// ingredients.forEach(ingredient => {
+//   const listItem = document.createElement('li');
+//   listItem.textContent = ingredient;
+//   listItem.classList.add('item');
+//   ingredientsList.appendChild(listItem);
+// });
 
-// 3
+// // 3
 
-const images = [
-  {
-    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
-    alt: "White and Black Long Fur Cat",
-  },
-  {
-    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
-    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
-  },
-  {
-    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
-    alt: "Group of Horses Running",
-  },
-];
+// const images = [
+//   {
+//     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+//     alt: "White and Black Long Fur Cat",
+//   },
+//   {
+//     url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+//     alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+//   },
+//   {
+//     url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+//     alt: "Group of Horses Running",
+//   },
+// ];
 
-const gallery = document.querySelector('.gallery');
+// const gallery = document.querySelector('.gallery');
 
-const galleryItems = images.map(image => {
-  return `<li><img src="${image.url}" alt="${image.alt}"></li>`;
-});
+// const galleryItems = images.map(image => {
+//   return `<li><img src="${image.url}" alt="${image.alt}"></li>`;
+// });
 
-gallery.insertAdjacentHTML('beforeend', galleryItems.join(''));
+// gallery.insertAdjacentHTML('beforeend', galleryItems.join(''));
 
 
 // 4
