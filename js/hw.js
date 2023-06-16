b8f1ea9b4147d4a043efc9130ee58581d95a6d19
Ostap <Ostapkriven13@gmail.com>
2023-06-16 20:16:59 +0300

    ss

--- js/hw.js
+++ js/hw.js
@@ -110,3 +110,12 @@ input.addEventListener("blur", () => {
   }
 });
 
+// 7
+const fontSizeControl = document.getElementById("font-size-control");
+const text = document.getElementById("text");
+
+fontSizeControl.addEventListener("input", () => {
+  const fontSize = fontSizeControl.value + "px";
+  text.style.fontSize = fontSize;
+});
+
