121c87d94a48aaba314a2a500e76aceab39c17b4
Ostap <Ostapkriven13@gmail.com>
2023-06-16 20:21:15 +0300

    s
    s

--- js/hw.js
+++ js/hw.js
@@ -111,11 +111,35 @@ input.addEventListener("blur", () => {
 });
 
 // 7
+
 const fontSizeControl = document.getElementById("font-size-control");
 const text = document.getElementById("text");
 
 fontSizeControl.addEventListener("input", () => {
   const fontSize = fontSizeControl.value + "px";
   text.style.fontSize = fontSize;
+});
+
+// 8
+
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  const emailInput = loginForm.elements.email;
+  const passwordInput = loginForm.elements.password;
+
+  if (emailInput.value === '' || passwordInput.value === '') {
+    alert('Please fill in all fields');
+  } else {
+    const loginData = {
+      email: emailInput.value,
+      password: passwordInput.value
+    };
+
+    console.log(loginData);
+    loginForm.reset();
+  }
 });
 
