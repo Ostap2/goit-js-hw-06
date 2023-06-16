3c1608f74da1d631309c60be60e55a3078529eae
Ostap <Ostapkriven13@gmail.com>
2023-06-16 19:57:46 +0300

    s

--- js/hw.js
+++ js/hw.js
@@ -76,3 +76,16 @@ incrementButton.addEventListener("click", () => {
   currentValue += 1;
   counterValue.textContent = currentValue;
 });
+// 5
+const nameInput = document.getElementById("name-input");
+const nameOutput = document.getElementById("name-output");
+
+nameInput.addEventListener("input", () => {
+  const name = nameInput.value.trim();
+
+  if (name === "") {
+    nameOutput.textContent = "Anonymous";
+  } else {
+    nameOutput.textContent = name;
+  }
+});
